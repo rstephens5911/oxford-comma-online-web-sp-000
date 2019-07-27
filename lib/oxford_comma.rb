983f54a13a0ca8a699cedb6ba72dff18ca46cfd9
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-27 00:13:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     array.join(", ")
 
-  elsif array.length == 2
+  elsif array.length > 1 && array.length < 2
     array.join(" and ")
 
   elsif array.length > 2
